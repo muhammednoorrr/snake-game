28ac995962792f38c7cf29b4bf48866a59060d6b
Muhammed <muhamme7d7@gmail.com>
2024-12-25 21:58:29 -0900

    clear board

--- index.js
+++ index.js
@@ -50,7 +50,10 @@ function nextTick(){
         displayGameOver();
     }
 };
-function clearBoard(){};
+function clearBoard(){
+    ctx.fillStyle = boardBackground;
+    ctx.fillRext(0, 0, gameWidth, gameHeight);
+};
 function createFood(){
     function randomFood(min, max){
         const randNum = Math.round((Math.random() * (max - min)+ min) / unitSize) * unitSize;
