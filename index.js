dc43e9258cf2a8addc000866ab6b4c0de42acb18
Muhammed <muhamme7d7@gmail.com>
2024-12-31 23:07:09 -0900

    displaygameover

--- index.js
+++ index.js
@@ -140,6 +140,12 @@ function checkGameOver(){
         }
     }
 };
-function displayGameOver(){};
+function displayGameOver(){
+    ctx.font = "50px MV Boli";
+    ctx.fillStyle = "black";
+    ctx.textAlign = "center";
+    ctx.fillText("GAME OVER!", gameWidth / 2,gameHeight / 2);
+    running = false;
+};
 function resetGame(){};
 //24:22
