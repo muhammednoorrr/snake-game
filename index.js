c08eba6c4901c4f7382d3164340faa5961e742a9
Muhammed <muhamme7d7@gmail.com>
2024-12-31 23:11:05 -0900

    resetgame

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ let snake = [
     {x:unitSize*2, y:0},
     {x:unitSize, y:0},
     {x:0, y:0}
-]
+];
     
 window.addEventListener("keydown", changeDirectioin);
 resetBtn.addEventListener("click", resetGame);
@@ -147,5 +147,16 @@ function displayGameOver(){
     ctx.fillText("GAME OVER!", gameWidth / 2,gameHeight / 2);
     running = false;
 };
-function resetGame(){};
-//24:22
+function resetGame(){
+    score = 0;
+    xVelocity = unitSize;
+    yVelocity = 0;
+    snake = [
+        {x:unitSize*4, y:0},
+        {x:unitSize*3, y:0},
+        {x:unitSize*2, y:0},
+        {x:unitSize, y:0},
+        {x:0, y:0}
+    ];
+    gameStart();
+};
